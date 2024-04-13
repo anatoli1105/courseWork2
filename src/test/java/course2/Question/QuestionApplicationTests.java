0f3ab5cc79a6e1b94af9b3ef4f983929f82c261c
anatolii <anatoliise@mail.ru>
2024-04-13 23:13:44 +0300

    comit

---
+++ src/test/java/course2/Question/QuestionApplicationTests.java
@@ -0,0 +1,12 @@
+package course2.Question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionApplicationTests {
+	@Test
+    void contextLoads() {
+    }
+
+}
